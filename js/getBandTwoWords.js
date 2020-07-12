dd36ba82c4e613a4ab7e360211f8bf89d281b74b
oharelbs <63164183+oharelbs@users.noreply.github.com>
2020-07-12 20:48:09 +0300

    moving JS and css
    to the correct folders

---
+++ js/getBandTwoWords.js
@@ -0,0 +1,13 @@
+function getBandTwoWords(element) {
+    
+    var boxchecked = element.checked;
+        if(boxchecked == false) {
+            document.getElementById('divwl').value = "";  
+        } else {
+            document.getElementById('band3').checked = false;
+            document.getElementById('divwl').value = "";
+            jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/band2/band2.txt', function(data) {
+            document.getElementById('divwl').value = data;
+        });
+    }
+}
