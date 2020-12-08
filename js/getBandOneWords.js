79e8524b902daead632727b9956ce975a49e97f7
oharelbs <63164183+oharelbs@users.noreply.github.com>
2020-12-08 13:52:39 +0200

    band 1 prod - js and html

--- js/getBandOneWords.js
+++ js/getBandOneWords.js
@@ -1,13 +1,13 @@
-function getBandTwoWords(element) {
+function getBandOneWords(element) {
     
     var boxchecked = element.checked;
         if(boxchecked == false) {
             document.getElementById('divwl').value = "";  
         } else {
-            document.getElementById('band1').checked = false;
+            document.getElementById('band2').checked = false;
             document.getElementById('band3').checked = false;
             document.getElementById('divwl').value = "";
-            jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/band2/band2.txt', function(data) {
+            jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/band1/band1.txt', function(data) {
             document.getElementById('divwl').value = data;
         });
     }
