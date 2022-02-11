03b27bb38c166afdd98119686b66f0caa1dc017b
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-02-12 00:18:31 +0200

    changes to DEV2
    adding an option to upload a file and parse it. currently, only text file

---
+++ DEV2/loadDoc.js
@@ -0,0 +1,7 @@
+function loadDoc() {
+
+    // creates the word object
+    <>
+        
+        <input type="file" id="imgupload" /><a href="#" onclick="$('#imgupload').trigger('click'); return false;">Upload file</a></>
+}
