84f435be13f838de1fbd12e3910beae851df1c49
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-02-24 22:16:38 +0200

    Band two band three
    disabling band I

--- js/getBandTwoWords.js
+++ js/getBandTwoWords.js
@@ -4,7 +4,7 @@ function getBandTwoWords(element) {
         if(boxchecked == false) {
             document.getElementById('divwl').value = "";  
         } else {
-            document.getElementById('band1').checked = false;
+//            document.getElementById('band1').checked = false;
             document.getElementById('band3').checked = false;
             document.getElementById('divwl').value = "";
             jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/band2/band2.txt', function(data) {
