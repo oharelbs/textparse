8a27ff22d5aa8515300b8eef7be8b0df71c557c7
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-10-26 19:53:04 +0300

    lists

--- DEV3/js/clearList.js
+++ DEV3/js/clearList.js
@@ -1,4 +1,9 @@
 function clearList() {
-document.getElementById('band2').checked = false;
-document.getElementById('lista').checked = false;
+    document.getElementById("divwl").innerHTML = "";
+    document.getElementById('band2').checked = false;
+    document.getElementById('band3').checked = false;
+    document.getElementById('lista').checked = false;
+    document.getElementById('listb').checked = false;
+    document.getElementById('listc').checked = false;
+    document.getElementById('listd').checked = false;
 }
