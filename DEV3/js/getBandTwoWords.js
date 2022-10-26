da0f0a16957ed3b05be288e84e500ec6db3595a9
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-10-26 21:09:08 +0300

    Dev3 redirect band words to master

--- DEV3/js/getBandTwoWords.js
+++ DEV3/js/getBandTwoWords.js
@@ -7,7 +7,7 @@ function getBandTwoWords(element) {
 //            document.getElementById('band1').checked = false;
             document.getElementById('band3').checked = false;
             document.getElementById('divwl').value = "";
-            jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/band2/band2.txt', function(data) {
+            jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/bandWords/band2.txt', function(data) {
             document.getElementById('divwl').value = data;
         });
     }
