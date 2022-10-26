7b4d51b7c1853bc05e749904167538570176d2dc
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-10-26 21:17:09 +0300

    Copying lists to Master

--- DEV3/js/getBandOneWords.js
+++ DEV3/js/getBandOneWords.js
@@ -2,7 +2,17 @@ function getBandOneWords(element) {
     
     var boxchecked = element.checked;
         if(boxchecked == false) {
-            document.getElementById('divwl').value = "";  
+            if(document.getElementById('band2').checked = true) {
+                document.getElementById('divwl').value = "";
+                jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/bandWords/band2.txt', function(data) {
+                    document.getElementById('divwl').value = data;
+                }
+            }
+
+
+
+
+            
         } else {
             document.getElementById('band2').checked = false;
             document.getElementById('band3').checked = false;
