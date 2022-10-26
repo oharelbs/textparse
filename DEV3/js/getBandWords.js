63a835a1ccce67231c0f76eda207b47c4890fe71
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-10-26 23:35:40 +0300

    Update getBandWords.js

--- DEV3/js/getBandWords.js
+++ DEV3/js/getBandWords.js
@@ -80,7 +80,7 @@ function getBandThreeWords(element) {
             if (existingData == '') {
                 document.getElementById('divwl').value = data;
             } else {
-                data = data.match(/^\s*\n/gm)
+                //data = data.match(/^\s*\n/gm)
                 document.getElementById('divwl').value = existingData + '\n' + data;
             }
         });
