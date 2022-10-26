3e5ef1a4f92a4016beda4159ce4243efa1199a55
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-10-26 23:20:30 +0300

    band3 link master

--- js/getBandThreeWords.js
+++ js/getBandThreeWords.js
@@ -6,8 +6,12 @@ function getBandThreeWords(element) {
 //            document.getElementById('band1').checked = false;
             document.getElementById('band2').checked = false;
             document.getElementById('divwl').value = "";
-            jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/bandWords/band3.txt', function(data) {
+//            jQuery.get('https://raw.githubusercontent.com/oharelbs/textparse/master/bandWords/band3.txt', function(data) {
+        jQuery.get('bandWords/band3.txt', function(data) {
             document.getElementById('divwl').value = data;
         });
     }
 }
+
+// https://github.com/oharelbs/textparse/blob/907fcc9057b03ed43bc431c5d931eef45438d37d/bandWords/band3.txt
+// bandWords/band3.txt
