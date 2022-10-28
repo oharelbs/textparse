416287434aa494ee6a66910879298b075d4ebd83
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-10-28 16:32:42 +0300

    Fixed copy with style

--- DEV3/js/copyHTMLFunction.js
+++ DEV3/js/copyHTMLFunction.js
@@ -1,18 +1,14 @@
 function copyHTMLFunction(containerid) {
 
   //https://stackoverflow.com/questions/36639681/how-to-copy-text-from-a-div-to-clipboard
- /* if (document.selection) { alert('line4')
-    var range = document.body.createTextRange();
-    range.moveToElementText(document.getElementById('divca'));
-    range.select().createTextRange();
-    document.execCommand("copy");
-  } else 
-  */
- if (window.getSelection) {
+  if (window.getSelection) {
+
     var range = document.createRange();
     range.selectNode(document.getElementById(containerid));
-    window.getSelection().addRange(range);
+    window.getSelection().removeAllRanges(); // clear current selection
+    window.getSelection().addRange(range); // to select text
     document.execCommand("copy");
+    window.getSelection().removeAllRanges();// to deselect
     alert("Text has been copied, now paste it somewhere.");
   }
 }
