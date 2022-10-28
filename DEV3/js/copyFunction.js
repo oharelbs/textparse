274c13bcae580ccab50545f69e63bd8bd562ac3f
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-10-28 18:03:50 +0300

    Copy Text buttons

--- DEV3/js/copyFunction.js
+++ DEV3/js/copyFunction.js
@@ -1,22 +1,31 @@
 function copyFunction(id) {
     //this copies a regular textarea field
-    if (id == "divwl") {
-
-        var copyText = document.getElementById(id);
-        copyText.select();
-        document.execCommand("copy");
+    if (id == "divwl") { //divwl is a text field, while the other are divs or contenteditable fields
+        if (document.getElementById(id).value.length == 0) {
+            alert('Nothing to copy.');
+        } else {
+            var copyText = document.getElementById(id);
+            copyText.select();
+            document.execCommand("copy");
+            alert("Text has been copied, now paste it somewhere.");
+        }
     } else {
         //this copies a div field
-        var copiedFrom = document.getElementById(id);
-        var copiedText = copiedFrom.innerText;
-        var myTempTexArea = document.createElement("textarea");
-        myTempTexArea.width = "1px";
-        myTempTexArea.height = "1px";
-        myTempTexArea.background = "transparent";
-        myTempTexArea.value = copiedText;
-        document.body.append(myTempTexArea);
-        myTempTexArea.select();
-        document.execCommand('copy');
-        document.body.removeChild(myTempTexArea);
+        if (document.getElementById(id).innerHTML == "") {
+            alert('Nothing to copy.');
+        } else {
+            var copiedFrom = document.getElementById(id);
+            var copiedText = copiedFrom.innerText;
+            var myTempTexArea = document.createElement("textarea");
+            myTempTexArea.width = "1px";
+            myTempTexArea.height = "1px";
+            myTempTexArea.background = "transparent";
+            myTempTexArea.value = copiedText;
+            document.body.append(myTempTexArea);
+            myTempTexArea.select();
+            document.execCommand('copy');
+            document.body.removeChild(myTempTexArea);
+            alert("Text has been copied, now paste it somewhere.");
+        }
     }
 }
