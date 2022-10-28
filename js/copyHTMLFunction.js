67c725c15f68adf30863668060ddff40864cf7de
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-10-28 16:45:48 +0300

    Copy-Paste fixed

--- js/copyHTMLFunction.js
+++ js/copyHTMLFunction.js
@@ -1,8 +1,7 @@
 function copyHTMLFunction(containerid) {
 
   //https://stackoverflow.com/questions/36639681/how-to-copy-text-from-a-div-to-clipboard
-  if (window.getSelection) {
-
+  
     var range = document.createRange();
     range.selectNode(document.getElementById(containerid));
     window.getSelection().removeAllRanges(); // clear current selection
@@ -10,5 +9,5 @@ function copyHTMLFunction(containerid) {
     document.execCommand("copy");
     window.getSelection().removeAllRanges();// to deselect
     alert("Text has been copied, now paste it somewhere.");
-  }
+
 }
