8100454f6de80d9b7d9f1acd004d630f3e596064
oharelbs <63164183+oharelbs@users.noreply.github.com>
2022-11-10 21:07:27 +0200

    band words

--- DEV3/js/getBandWords.js
+++ DEV3/js/getBandWords.js
@@ -24,7 +24,7 @@ function getWordList(element) {
 }
 
 function excludeWord(data) {
-    var excludedWords = ['as',  '\/\^out\n', 'so\n', 'the\n', 'or\n', '\/\^my\n']; // --> /^beginning of line \n - end of line. Ensures that the word is the only word in that line
+    var excludedWords = ['\/\^out\n', 'so\n', 'the\n', '\/\^or\n', '\/\^my\n']; // --> /^beginning of line \n - end of line. Ensures that the word is the only word in that line
     excludedWords = excludedWords.toString().split(','); //'for', 'in', 'one',
     for (var e = 0; e < excludedWords.length; e++) {
          data = data.replace(excludedWords[e], '')
